476d85c1abe7f2093e891196bec6a5a872556d3a
xhzj1314 <240314832@qq.com>
2025-06-01 01:18:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -39,7 +39,7 @@ const API_SITES = {
         name: '天涯资源',
     },
     // xiaomaomi: {
-    //     api: 'https://zy.xiaomaomi.cc/api.php/provide/vod'，
+    //     api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
     //     name: '小猫咪资源',
     // },
     ffzy: {
@@ -128,40 +128,40 @@ const API_SITES = {
          adult: false
      },
      jkun: {
-         api: 'https://jkunzyapi.com'，
+         api: 'https://jkunzyapi.com',
          name: 'jkun资源',
-         adult: false
+         adult: true
      },
      bwzy: {
          api: 'https://api.bwzym3u8.com'，
          name: '百万资源',
-         adult: false
+         adult: true
      },
      souav: {
          api: 'https://api.souavzy.vip'，
          name: 'souav资源',
-         adult: false
+         adult: true
      },
      r155: {
-         api: 'https://155api.com'，
+         api: 'https://155api.com',
          name: '155资源',
-         adult: false
+         adult: true
      },
      lsb: {
          api: 'https://apilsbzy1.com',
          name: 'lsb资源',
-         adult: false
+         adult: true
      },
      huangcang: {
          api: 'https://hsckzy.vip',
          name: '黄色仓库',
-         adult: false
+         adult: true,
          detail: 'https://hsckzy.vip'
      },
      yutu: {
          api: 'https://yutuzy10.com',
          name: '玉兔资源',
-         adult: false
+         adult: true
      },
 
     // 下面是资源失效率高的API源，不建议使用
@@ -174,7 +174,7 @@ const API_SITES = {
     //     name: '蜂巢资源'
     // },
     // ukzy: {
-    //     api: 'https://api.ukuapi88.com/api.php/provide/vod'，
+    //     api: 'https://api.ukuapi88.com/api.php/provide/vod',
     //     name: 'U酷资源'
     // },
 };
