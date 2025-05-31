cbea184eb14e1af8590371e3a15f347c4b5cd50c
xhzj1314 <240314832@qq.com>
2025-06-01 00:21:54 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -112,7 +112,7 @@ const API_SITES = {
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
-        adult: true
+        adult: false
     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
     // 互联网上传播的色情内容将人彻底客体化、工具化，是性别解放和人类平等道路上的巨大障碍。
@@ -125,7 +125,7 @@ const API_SITES = {
     // ckzy: {
     //     api: 'https://www.ckzy1.com',
     //     name: 'CK资源',
-    //     adult: true
+    //     adult: false
     // },
     // jkun: {
     //     api: 'https://jkunzyapi.com',
