066479c4da6c0f7215057d4f9bd6f3cd0e08b52d
xhzj1314 <240314832@qq.com>
2025-08-04 22:24:58 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -39,10 +39,10 @@ const API_SITES = {
         api: 'https://tyyszy.com/api.php/provide/vod',
         name: '天涯资源',
     },
-    xiaomaomi: {
-        api: 'https://zy.xmm.hk/api.php/provide/vod',
-        name: '小猫咪资源',
-    },
+     jinma: {
+         api: 'https://api.jmzy.com/api.php/provide/vod',
+         name: '金马资源',
+     },
     ffzy: {
         api: 'http://ffzy5.tv/api.php/provide/vod',
         name: '非凡影视',
@@ -110,15 +110,71 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
-    lzi: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod/',
-        name: '量子资源站'
-    },
     testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+        api: 'https://api.xiaojizy.live/provide/vod',
+        name: '小鸡资源',
         adult: true
     },
+     ckzy: {
+         api: 'https://www.ckzy1.com/api.php/provide/vod',
+         name: 'CK资源',
+         adult: true
+     },
+     jkun: {
+         api: 'https://jkunzyapi.com/api.php/provide/vod',
+         name: 'jkun资源',
+         adult: true
+     },
+     bwzy: {
+         api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+         name: '百万资源',
+         adult: true
+     },
+     souav: {
+         api: 'https://api.souavzy.vip/api.php/provide/vod',
+         name: 'souav资源',
+         adult: true
+     },
+     r155: {
+         api: 'https://155api.com/api.php/provide/vod',
+         name: '155资源',
+         adult: true
+     },
+     lsb: {
+         api: 'https://apilsbzy1.com/api.php/provide/vod',
+         name: 'lsb资源',
+         adult: true
+     },
+     huangcang: {
+         api: 'https://hsckzy.vip/api.php/provide/vod',
+         name: '黄色仓库',
+         adult: true,
+         detail: 'https://hsckzy.vip',
+     },
+     yutu: {
+         api: 'https://apiyutu.com/api.php/provide/vod',
+         name: '玉兔资源',
+         adult: true
+     },
+      sousuo: {
+         api: 'https://api.souavzy.vip/api.php/provide/vod',
+         name: '搜索资源',
+         adult: true
+     },
+
+    // 下面是资源失效率高的API源，不建议使用
+    // subo: {
+    //     api: 'https://subocaiji.com/api.php/provide/vod',
+    //     name: '速播资源'
+    // },
+    // fczy: {
+    //     api: 'https://api.fczy888.me/api.php/provide/vod',
+    //     name: '蜂巢资源'
+    // },
+    // ukzy: {
+    //     api: 'https://api.ukuapi88.com/api.php/provide/vod',
+    //     name: 'U酷资源'
+    // },
 };
 
 // 定义合并方法
