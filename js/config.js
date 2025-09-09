8757f8e80b1105e4a24abf824e526fdee852cb9f
xhzj1314 <240314832@qq.com>
2025-09-09 20:05:24 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,57 +110,7 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源',
     },
-    testSource: {
-        api: 'https://api.xiaojizy.live/provide/vod',
-        name: '小鸡资源',
-        adult: true
-    },
-     ckzy: {
-         api: 'https://www.ckzy1.com/api.php/provide/vod',
-         name: 'CK资源',
-         adult: true
-    },
-     jkun: {
-         api: 'https://jkunzyapi.com/api.php/provide/vod',
-         name: 'jkun资源',
-         adult: true
-     },
-     bwzy: {
-         api: 'https://api.bwzym3u8.com/api.php/provide/vod',
-         name: '百万资源',
-         adult: true
-     },
-     souav: {
-         api: 'https://api.souavzy.vip/api.php/provide/vod',
-         name: 'souav资源',
-         adult: true
-     },
-     r155: {
-         api: 'https://155api.com/api.php/provide/vod',
-         name: '155资源',
-         adult: true
-     },
-     lsb: {
-         api: 'https://apilsbzy1.com/api.php/provide/vod',
-         name: 'lsb资源',
-         adult: true
-     },
-     huangcang: {
-         api: 'https://hsckzy.vip/api.php/provide/vod',
-         name: '黄色仓库',
-         detail: 'https://hsckzy.vip',
-         adult: true
-     },
-     yutu: {
-         api: 'https://apiyutu.com/api.php/provide/vod',
-         name: '玉兔资源',
-         adult: true
-     },
-      sousuo: {
-         api: 'https://api.souavzy.vip/api.php/provide/vod',
-         name: '搜索资源',
-         adult: true
-     },
+   
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
