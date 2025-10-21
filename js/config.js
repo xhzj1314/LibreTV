ccca1d5b029fbb8eda7ca6c5cfc097f5d8d737c8
xhzj1314 <240314832@qq.com>
2025-10-21 06:44:46 +0200

    Update API URLs in config.js

--- js/config.js
+++ js/config.js
@@ -33,7 +33,7 @@ const API_SITES = {
         detail: 'http://caiji.dyttzyapi.com', 
     },
      dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        api: 'https://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
     },
@@ -121,7 +121,7 @@ const API_SITES = {
         name: 'iKun资源'
     },
     xiaoji: {
-        api: 'https://api.xiaojizy.live/provide/vod',
+        api: 'https://api.xiaojizy.live/api.php/provide/vod',
         name: '小鸡资源',
         adult: true
     },
@@ -139,11 +139,6 @@ const API_SITES = {
          api: 'https://api.bwzym3u8.com/api.php/provide/vod',
          name: '百万资源',
          adult: true
-     },
-     souav: {
-        api: 'https://api.souavzy.vip/api.php/provide/vod',
-        name: 'souav资源',
-        adult: true
      },
      r155: {
          api: 'https://155api.com/api.php/provide/vod',
@@ -171,7 +166,7 @@ const API_SITES = {
          adult: true
       },
     zuise: {
-         api: 'https://api.zuiseapi.com/api.php/provide/vod/',
+         api: 'https://api.zuiseapi.com/api.php/provide/vod',
          name: '最色资源',
          adult: true
        }, 
